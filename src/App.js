abbab1bf8b6fae549665981eafa4cce286c2a326
Akshay7-max <52664007+Akshay7-max@users.noreply.github.com>
2022-07-16 23:51:44 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import "./App.css";
+import axios from "axios";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 
-function App() {
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+  const [move, setMove] = useState(0);
+
+  const getData = async() =>{
+      const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${count}`);
+      console.log(res);
+      setName(res.data.name);
+      setMove(res.data.moves.length);
+  } 
+
+  useEffect(() => {
+    getData();
+  }, [count])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <h1>You have select <span style={{ color: "blueviolet" }}>{count}</span> number.</h1>
+          <h1>Your selected character is <span style={{ color: "teal" }}>{name}</span> .</h1>
+          <h1>Having number of moves is <span style={{ color: "deeppink" }}>{move}</span> .</h1>
+          <select value={count} onChange={(event) => { setCount(event.target.value) }}>
+            <option value="1">1</option>
+            <option value="25">25</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+          </select>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
